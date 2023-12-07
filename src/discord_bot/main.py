43470730c800d6ed98665005e9f852fbd1ec1509
jichi <Jichi.Guo@gmail.com>
2023-12-07 15:11:06 -0600

    update

--- src/discord_bot/main.py
+++ src/discord_bot/main.py
@@ -1,13 +1,17 @@
 from os import getenv
+from typing import List
+from random import choice
 
 from discord import Intents
 from discord import Message
 from discord.ext.commands import Bot
+from discord.ext.commands import has_role
 from discord.utils import setup_logging
 from discord.utils import find
 from discord import Member
 from structlog import get_logger
 from typeguard import typechecked
+from discord.ext.commands.errors import CheckFailure
 
 from .hello import hellomain
 from .types import P
@@ -24,6 +28,22 @@ async def bot(token:str, guild:str)->None:
     @typechecked
     async def on_ready()->None:
         await logger.ainfo('%s has connected to Discord!', bot.user.name)
+        # TODO get list of games from rest api
+    # TODO admin command to reload list of games
+
+    @bot.event
+    @typechecked
+    async def on_command_error(ctx, error)->None:
+        if isinstance(error, CheckFailure):
+            await ctx.send(error)
+        await logger.aexception(error)
+
+    @bot.command(name='shutdown')
+    @has_role('admin')
+    @typechecked
+    async def shutdown(ctx)->None:
+        await ctx.send('Disconnecting bot')
+        return await bot.close()
 
     @bot.command(name='99')
     @typechecked
@@ -37,7 +57,7 @@ async def bot(token:str, guild:str)->None:
             ),
         ]
 
-        response:str = random.choice(brooklyn_99_quotes)
+        response:str = choice(brooklyn_99_quotes)
         await ctx.send(response)
 
     return await bot.start(token)
